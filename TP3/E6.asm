5ea97f3f8a95a47978e644b017d6de8387373ff2
amoussa.rayan77130@hotmail.fr <amoussa.rayan77130@hotmail.fr>
2024-10-11 19:45:33 +0200

    TP3: exo6 fini

--- TP3/E6.asm
+++ TP3/E6.asm
@@ -1,21 +1,48 @@
 %include "asm_io.inc"
 
 SECTION .data
-prompt1 : db "Entrer un entier strictement positif: ", 0
-
+prompt1: db "Entrer un entier strictement positif: ", 0
 
 SECTION .bss
-input1 : resd 1
+input1: resd 1          ; variable du nombre dont on cherche tous les diviseurs
+diviseur: resd 1        ; variable du diviseur
 
 SECTION .text
-global  main
-main :
+global main
+main:
+    ;AFFICHER LA QUESTION ET CAPTURER LA REPONSE
     mov eax, prompt1
-    call print_string     ; Affichage de prompt1.
-    call read_int         ; Lecture d'un entier.
-    mov [input1], eax
-    call print_int
+    call print_string     
+    call read_int         
+    mov [input1], eax     
 
-    mov ebx, 0
-    mov eax, 1
-    int 0x80
+    mov dword [diviseur], 1   ;Initialiser le diviseur à 1
+
+division:
+    ;EFFECTUER LA DIVISION
+    mov eax, [input1]     
+    mov ecx, [diviseur]   
+    div ecx               
+
+    ; VERIFIER LE RESTE DE LA DIVISION
+    cmp edx , 0
+    jg increment
+    mov eax, [diviseur] 
+    ;AFFICHER SI RESTE=0
+    call print_int
+    call print_espace
+
+increment:
+    ; INCREMENTER DE 1 LE DIVISEUR
+    inc dword [diviseur]  ; Increment the divisor.
+    mov edx , 0
+    mov eax, [input1]     
+    mov ecx, [diviseur]  
+    cmp   ecx , eax
+    jng division
+    
+    ;ARRETER LE PROGRAMME
+    call print_nl
+    mov eax, 1            ; Exit code for Linux system call.
+    mov ebx, 0            ; Status code 0.
+    int 0x80              ; Exit the program.
